6499300ee5d8ff78d8b6b670b9247c3ded6321b9
Brayan Gamboa <bsgv2005@gmail.com>
2023-03-05 17:50:12 -0500

    Agregar archivos de proyecto.

---
+++ Complejos.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad_2
+{
+    internal class Complejos
+    {
+        double valorA, valorB, valorC, valorD;
+        List<double> list = new List<double>();
+        
+        //Creamos el constructor
+        public Complejos(double valorA, double valorB, double valorC, double valorD)
+        {
+            this.valorA = valorA;
+            this.valorB = valorB;
+            this.valorC = valorC;
+            this.valorD = valorD;
+        }
+
+        //Encapsulamos las variables, para que sean propiedades de solo lectura
+        public double ValorA { get => valorA; }
+        public double ValorB { get => valorB; }
+        public double ValorC { get => valorC; }
+        public double ValorD { get => valorD; }
+
+
+        
+        //Procedemos a crear los diferentes métodos que realizaremos con los valores antes mencionados
+        public String Suma()
+        {
+            //Fórmula:
+            //(a, b) + (c, d) = (a + c, b + d);
+            double parteReal = valorA + valorC;
+            double parteImaginaria = valorB + valorD;
+
+            return $"La suma correspondiente es: ({Math.Round(parteReal,5)}, {Math.Round(parteImaginaria,5)}i)\n";
+        }
+
+        public String Resta()
+        {
+            //Fórmula:
+            //(a, b) - (c, d) = (a - c, b - d);
+            double parteReal = valorA - valorC;
+            double parteImaginaria = valorB - valorD;
+
+            return $"La resta correspondiente es: ({Math.Round(parteReal,5)}, {Math.Round(parteImaginaria,5)}i)\n";
+
+        }
+
+        public String Multiplicacion()
+        {
+            //Fórmula:
+            //(a, b) * (c, d) = (a * c – b* d, a*d + b * c)
+            double parteReal = valorA * valorC - valorB * valorD;
+            double parteImaginaria = valorA * valorD + valorB * valorC;
+
+            return $"La multiplicacion correspondiente es: ({Math.Round(parteReal,5)}, {Math.Round(parteImaginaria,5)}i)\n";
+
+        }
+        public String Division()
+        {
+            //Fórmula:
+            //(a, b) / (c, d) = ((a * c + b * d) / (c2 + d2), (b * c – a* d) / (c2 + d2))
+            double parteReal = (valorA * valorC + valorB * valorD) / (Math.Pow(valorC, 2) + Math.Pow(valorD, 2));
+            double parteImaginaria = (valorB * valorC - valorA * valorD) / (Math.Pow(valorC, 2) + Math.Pow(valorD, 2));
+
+            return $"La división correspondiente es: \n  {Math.Round(parteReal,5)}\n-------------\n  {Math.Round(parteImaginaria,5)}i\n";
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Reflection;
+
+namespace Actividad_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string respuesta = "1";
+
+            //Con este ciclo While lograremos repetir el código las ocasiones que el usuario desee
+            while (respuesta == "1")
+            {
+                Console.Clear();
+                double[] valorUsuario = new double[4];
+                string letra = "";
+                int bandera = 1;
+
+                //Si el usuario se equivocó presionando una letra y no un número, este condicional repite el proceso para que el usuario ingrese los daos nuevamente
+                while (bandera == 1)
+                {
+
+                    for (int i = 0; i < 4; i++)
+                    {
+                        switch (i)
+                        {
+                            case 0:
+                                letra = "A";
+                                break;
+                            case 1:
+                                letra = "B";
+                                break;
+                            case 2:
+                                letra = "C";
+                                break;
+                            case 3:
+                                letra = "D";
+                                break;
+                        }
+
+                        Console.WriteLine($"Por favor ingresa el valor {letra}");
+                        try
+                        {
+                            valorUsuario[i] = double.Parse(Console.ReadLine());
+                            bandera = 0;
+                        }
+                        catch
+                        {
+                            Console.BackgroundColor = ConsoleColor.Red;
+                            Console.WriteLine("¡Error!, se ha ingresado un valor no numérico");
+                            Console.ResetColor();
+                            Thread.Sleep(2500);
+                            Console.Clear();
+                            Console.WriteLine("Intentemos nuevamente");
+                            bandera = 1;
+                            break;
+                        }
+                    }
+                }
+
+                //Creamos nuestro objeto con nombre "operacion1" en base a la clase Complejor, con los datos que le pedimos al usuario.
+                Complejos operacion1 = new Complejos(valorUsuario[0], valorUsuario[1], valorUsuario[2], valorUsuario[3]);
+
+                Console.WriteLine(operacion1.Suma());
+                Console.WriteLine(operacion1.Resta());
+                Console.WriteLine(operacion1.Multiplicacion());
+                Console.WriteLine(operacion1.Division());
+
+                Console.WriteLine("¿Desea volver a realizar otra operación?\n - Marque 1 para hacer otra operación. \n - Marque cualquier carácter para finalizar");
+                respuesta = Console.ReadLine();
+
+            }
+        }
+    }
+}
